5d5c83336f6c63d8ef0e76efe03b65a58fbb25fc
gal.yossipov <gal.yossipov@anecdotes.ai>
2023-12-06 12:37:47 +0200

    added main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import sys
 def save_input_to_file(s):
     with open('output.txt', 'w+') as f:
         f.write(s)
-    logging.info(f"Created output.txt file with content {s[:10]}...")
+    print(f"Created output.txt file with content {s[:10]}...")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     args = sys.argv
     if len(sys.argv) < 2:
-        logging.error("No string input was given")
+        print("No string input was given")
         exit(1)
     save_input_to_file(args[1])
 
